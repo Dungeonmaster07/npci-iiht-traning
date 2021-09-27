abecebe2b9226a4008a1b5dccdc8cddf4c0557fa
Subham Goswami <goswamisubham0706@gmail.com>
2021-09-27 11:36:46 +0530

    Python Assignment 1

---
+++ Assignment1.py
@@ -0,0 +1,15 @@
+no_of_students=int(input("Enter the number of students: "))
+physics_marks = int(input("Enter the marks of Physics: "))
+maths_marks=int(input("Enter the marks of Maths: "))
+chemistry_marks=int(input("Enter the marks of Chemistry: "))
+
+if physics_marks>50 and chemistry_marks>50 and maths_marks>50 : 
+    print("Congratulations! You pass bruh..")
+elif physics_marks>50 and chemistry_marks>50 and maths_marks<=50:
+    print("You pass but please re-appear for maths exam..")
+elif physics_marks>50 and chemistry_marks<=50 and maths_marks>50:
+    print("You pass but please re-appear for chemistry exam..")
+elif physics_marks<=50 and chemistry_marks>50 and maths_marks>50:
+    print("You pass but please re-appear for physics exam..")
+else:
+    print("Sorry bruh You failed..Please study hard")
